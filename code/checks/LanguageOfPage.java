a1a4c8123e19a1379c7c1cac5f52d6e116e9d247
fstoqnov <fstoqnov@gmail.com>
2018-10-30 13:09:30 +0000

    Addidtional checks
    Added check for Language of Page
    Added check for Labels or instructions (inclomplete)

---
+++ code/checks/LanguageOfPage.java
@@ -0,0 +1,55 @@
+package code.checks;
+
+import code.selenium_interface.Interface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import org.openqa.selenium.WebElement;
+
+public class LanguageOfPage extends Check {
+	
+	private ArrayList<String> lang;
+	
+	protected LanguageOfPage() {
+		super("Criterion 3.1.1 Language of Page");
+	}
+
+	@Override
+	public void runCheck(String urlContent, List<Marker> markers, Interface inter) {
+		WebElement[] doc = inter.getElementsByTagName("html");
+		for (int i = 0; i < doc.length; i ++) {
+			if(doc[i].getAttribute("language") == null) {
+				addFlagToElement(markers, Marker.MARKER_ERROR, doc[i]);
+			} else if(!lang.contains(doc[i].getAttribute("language"))){
+				addFlagToElement(markers, Marker.MARKER_ERROR, doc[i]);;
+			}
+			addFlagToElement(markers, Marker.MARKER_SUCCESS, doc[i]);
+		}
+		
+	}
+
+	@Override
+	public String[] getHTMLPass() {
+		return new String[] {
+				"<html lang=\"language\"></html>"
+		};
+	}
+
+	@Override
+	public String[] getHTMLFail() {
+		return new String[] {
+				"<html lang=\"not-included\"></html>"
+		};
+	}
+
+	@Override
+	public void initialise() {
+		lang = new ArrayList<String>();
+		lang.addAll(Arrays.asList(Locale.getISOLanguages()));
+		
+	}
+
+}
