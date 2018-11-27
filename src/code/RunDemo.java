a4b81de5745545693fc7ce3756c00dc02e490fa4
MattWarren97 <mfw1g15@soton.ac.uk>
2018-11-27 15:32:49 +0000

    ConformanceReport on RunDemo is slightly broken - this is the case that breaks it

--- src/code/RunDemo.java
+++ src/code/RunDemo.java
@@ -8,10 +8,11 @@ public class RunDemo {
 		CheckList cl = new CheckList();
 		DatabaseInterface db;
 		try {
-			//cl.runChecksAtURLs(new String[] { "https://ssl.bbc.co.uk/faqs/forms" }, true, db = new DatabaseInterface(tests.interfaces.DatabaseInterface.connString), false);
-			//cl.runChecksAtURLs(new String[] { "https://google.co.uk" }, true, db = new DatabaseInterface(tests.interfaces.DatabaseInterface.connString), false);
-			cl.runChecksAtURLs(new String[] { "https://www.wufoo.com/html5/tabindex-attribute/" }, true, db = new DatabaseInterface(tests.interfaces.DatabaseInterface.connString), false);
-			new ConformanceReport().generateReportFromPage(db, "google.com", new SeleniumInterface());
+			//String testURL = "https://google.co.uk";
+			//String testURL = "https://ssl.bbc.co.uk/faqs/forms";
+			String testURL = "https://www.wufoo.com/html5/tabindex-attribute/";
+			cl.runChecksAtURLs(new String[] {testURL}, true, db = new DatabaseInterface(tests.interfaces.DatabaseInterface.connString), false);
+			new ConformanceReport().generateReportFromPage(db, testURL, new SeleniumInterface());
 			//cl.runChecksAtURLs(new String[] { "https://www.guru99.com/execute-javascript-selenium-webdriver.html" }, true, db = new DatabaseInterface(tests.interfaces.DatabaseInterface.connString), true);
 			//new ConformanceReport().generateReportFromPage(db, "guru99.com");
 		} catch (Exception e) {
