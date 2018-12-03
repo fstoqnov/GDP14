e70173827b1fc074fe4da1333c58cd971432243a
MattWarren97 <mfw1g15@soton.ac.uk>
2018-12-03 19:33:30 +0000

    temp version - fixing HeadingsAndLabels

--- src/code/checks/HeadingsAndLabels.java
+++ src/code/checks/HeadingsAndLabels.java
@@ -17,27 +17,40 @@ public class HeadingsAndLabels extends Check {
 
 	@Override
 	public void runCheck(String urlContent, List<Marker> markers, SeleniumInterface inter) {
+		List<WebElement> allElements = inter.getAllElements();
+
+		checkSiblingHeadingsUnique(inter);
 		//Sibling headings must be unique
-		int startLevel = 1;
-		WebElement parent = null;
-		boolean uniqueSiblingHeaders = checkSiblingHeaderUniqueness(startLevel, parent, inter);
+		//int startLevel = 1;
+		//WebElement parent = null;
+		//boolean uniqueSiblingHeaders = checkSiblingHeaderUniqueness(startLevel, parent, inter);
 		
 
 	}
 	
-	private boolean checkSiblingHeaderUniqueness(int level, WebElement parent, SeleniumInterface inter) {
-		WebElement[] headers;
-		
+	private boolean checkSiblingHeadingsUnique(int level, List<WebElement> allElements, SeleniumInterface inter) {
+		//check that headings are unique,
 		String headerLevel = "h" + String.valueOf(level); //eg level 1 = "h1"
-		if (level == 1) { //no parent, so can't use 'getsubelements'
-			headers = inter.getElementsByTagName(headerLevel);
-		}
-		else {
-			headers = inter.getSubElementsByTagName(parent, headerLevel);
-		}
-		System.out.println("Number of " + headerLevel + " elements found is " + String.valueOf(headers.length));
+		String nextHeaderLevel = "h" + String.valueOf(level+1);
 
+		//can use getAllElements to find all elements - then find all h1,h2,h3,h4 etc by tag name.
+		//then can use allElements.indexOf(myElement) to work out how all those elements were ordered...
+		//surely there's a better way though... 
+		//maybe not...
+		
+		WebElement[] headers = inter.getElementsByTagName(headerLevel);
+		WebElement[] nextHeaders = inter.getElementsByTagName(nextHeaderLevel);
+		
+		System.out.println("Number of " + headerLevel + " elements found is " + String.valueOf(headers.length));
+		
+		
+		
+		
+		
 		ArrayList<String> headerLabels = new ArrayList<String>();
+		
+		
+		
 		for (int i = 0; i < headers.length; i++) {
 			//check uniqueness of sub-headers of child:
 			if (!checkSiblingHeaderUniqueness(level+1, headers[i], inter)) {
