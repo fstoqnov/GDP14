a4bac41c4cf511bff1f3fd47eb47bab18aed01ce
fstoqnov <fstoqnov@gmail.com>
2018-12-06 14:36:25 +0000

    Updated Language of Page
    Now accounts for all variances of languages (convention shows that the specification is always marked in capital letters)

--- src/code/checks/LanguageOfPage.java
+++ src/code/checks/LanguageOfPage.java
@@ -40,7 +40,7 @@ public class LanguageOfPage extends Check {
 	@Override
 	public String[] getHTMLPass() {
 		return new String[] {
-				"<html lang=\"en\"></html>"
+				"<html lang=\"en-GB\"></html>"
 		};
 	}
 
@@ -56,7 +56,86 @@ public class LanguageOfPage extends Check {
 	public void initialise() {
 		lang = new ArrayList<String>();
 		lang.addAll(Arrays.asList(Locale.getISOLanguages()));
-		
+		lang.add("ar-DZ");
+		lang.add("ar-BH");
+		lang.add("ar-EG");
+		lang.add("ar-IQ");
+		lang.add("ar-JO");
+		lang.add("ar-KW");
+		lang.add("ar-LB");
+		lang.add("ar-LY");
+		lang.add("ar-MA");
+		lang.add("ar-OM");
+		lang.add("ar-QA");
+		lang.add("ar-SA");
+		lang.add("ar-SY");
+		lang.add("ar-TN");
+		lang.add("ar-AE");
+		lang.add("ar-YE");
+		lang.add("az-AZ");
+		lang.add("zh-CN");
+		lang.add("zh-HK");
+		lang.add("zh-MO");
+		lang.add("zh-SG");
+		lang.add("zh-TW");
+		lang.add("nl-BE");
+		lang.add("nl-NL");
+		lang.add("en-AU");
+		lang.add("en-BZ");
+		lang.add("en-CA");
+		lang.add("en-CB");
+		lang.add("en-GB");
+		lang.add("en-IN");
+		lang.add("en-IE");
+		lang.add("en-JIM");
+		lang.add("en-NZ");
+		lang.add("en-PH");
+		lang.add("en-ZA");
+		lang.add("en-TT");
+		lang.add("en-US");
+		lang.add("fr-BE");
+		lang.add("fr-CA");
+		lang.add("fr-FR");
+		lang.add("fr-LU");
+		lang.add("fr-CH");
+		lang.add("gd-IE");
+		lang.add("de-AT");
+		lang.add("de-DE");
+		lang.add("de-LI");
+		lang.add("de-LU");
+		lang.add("de-CH");
+		lang.add("it-IT");
+		lang.add("it-CH");
+		lang.add("ms-BN");
+		lang.add("de-MY");
+		lang.add("no-NO");
+		lang.add("pt-BR");
+		lang.add("pt-PT");
+		lang.add("ro-MO");
+		lang.add("ru-MO");
+		lang.add("sr-SP");
+		lang.add("es-AR");
+		lang.add("es-BO");
+		lang.add("es-CL");
+		lang.add("es-CO");
+		lang.add("es-CR");
+		lang.add("es-DO");
+		lang.add("es-EC");
+		lang.add("es-SV");
+		lang.add("es-GT");
+		lang.add("es-HN");
+		lang.add("es-MX");
+		lang.add("es-NI");
+		lang.add("es-PA");
+		lang.add("es-PY");
+		lang.add("es-PE");
+		lang.add("es-PR");
+		lang.add("es-ES");
+		lang.add("es-UY");
+		lang.add("es-VE");
+		lang.add("sv-FI");
+		lang.add("sv-SE");
+		lang.add("uz-UZ");
+	
 	}
-
 }
