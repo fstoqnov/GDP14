bc3af976daf5abae436af1abe32b2164ce99523f
DESKTOP-2I44S7T\Aaron <aaron.tasker@gmail.com>
2019-01-30 13:41:38 +0000

    Added blank check for screenshotting

---
+++ src/code/checks/NewCheck.java
@@ -0,0 +1,20 @@
+package code.checks;
+
+import java.util.List;
+
+import code.Marker;
+import code.interfaces.SeleniumInterface;
+
+public class NewCheck extends Check {
+
+	protected NewCheck() {
+		super("Criterion Name");
+	}
+
+	public void runCheck(String urlContent, List<Marker> markers, SeleniumInterface inter) {  }
+
+	public void initialise() {  }
+
+	public void setupTests() {  }
+
+}
